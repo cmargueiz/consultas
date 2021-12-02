a2e1dffce0cb2480456fccf03981927b4beba6c3
cmargueiz <c.margueiz@gmail.com>
2021-12-02 17:16:32 -0600

    Consultas para factura terminadas

---
+++ Mis consultas/Arreglos de consultas de citas.sql
@@ -0,0 +1,17 @@
+select max(fv.idFacturaVenta) from facturasVentas fv ;
+
+select CONCAT(u.idusuario ,' - ',u.primerNombre,' ',u.segundoApellido) from usuario u where u.cargo =49;
+
+SELECT 
+rc.idCita,
+CONCAT(c.primerNombre,' ',c.primerApellido) as paciente,
+rc.horaReserva ,
+rc.fechaReserva ,
+concat(u.primerNombre,' ',c.segundoApellido) as medico,
+rc.estado
+from usuario u 
+inner join reservarCita rc 
+on u.idusuario = rc.medico_idUsuario 
+inner join cliente c 
+on rc.cliente_idcliente = c.idclientewhere u.Cargo = '49' ;
+
--- Mis consultas/ConsultasFactura.sql
+++ Mis consultas/ConsultasFactura.sql
@@ -1,16 +1,6 @@
 
-select 
-dp.cantidad ,
-(select p.nombreProducto from producto p where p.idproducto=dp.producto_idproducto) as producto,
-dp.precioUnitario ,
-dp.precioTotal 
-from facturasVentas fv 
-inner join detallePedido dp 
-on fv.pedidoVenta_idPedido = dp.pedidoVenta_idPedido 
-where fv.idFacturaVenta =3;
-
-
-
+declare @idFactura int = 56
+declare @total float
 declare @direccion varchar(255)
 set @direccion = (select 
 (select top 1 direccion from direccionesCliente dc where dc.cliente_idcliente= c.idcliente)
@@ -19,41 +9,47 @@ inner join pedidoVenta pv
 on c.idcliente = pv.cliente_idcliente 
 inner join facturasVentas fv 
 on pv.idPedido = fv.pedidoVenta_idPedido 
-where fv.idFacturaVenta =3)
-select 
-concat(c.primerNombre,' ',c.primerApellido) as nombre,
-case
-when @direccion is NULL
-then ''
-else @direccion
-end as direccion,
-CONVERT(varchar, fv.fechaReg, 103) as fecha,
-c.numeroDui 
+where fv.idFacturaVenta =@idFactura)
+set @direccion = (select 
+(select top 1 direccion from direccionesCliente dc where dc.cliente_idcliente= c.idcliente)
 from cliente c 
 inner join pedidoVenta pv 
 on c.idcliente = pv.cliente_idcliente 
 inner join facturasVentas fv 
 on pv.idPedido = fv.pedidoVenta_idPedido 
-where fv.idFacturaVenta =8
-
-
-declare @idFactura int
-set @idFactura = 3
+where fv.idFacturaVenta =@idFactura)
+set @total= (
+	select sum(dp.precioTotal) from facturasVentas fv2
+	inner join pedidoVenta pv2 
+	on fv2.pedidoVenta_idPedido = pv2.idPedido 
+	inner join detallePedido dp 
+	on pv2.idPedido = dp.pedidoVenta_idPedido
+	where fv2.idFacturaVenta=@idFactura) 
 select 
 dp.pedidoVenta_idPedido ,
 (select concat(c.primerNombre,' ',c.primerApellido) from cliente c where c.idcliente=pv.cliente_idcliente) as cliente,
 (select c.numeroDui from cliente c where c.idcliente=pv.cliente_idcliente) as dui,
+case
+when @direccion is NULL
+then ''
+else @direccion
+end as direccion,
 dp.cantidad ,
 (select p.nombreProducto from producto p where p.idproducto=dp.producto_idproducto) as producto,
 dp.precioUnitario ,
-dp.precioTotal 
+dp.precioTotal ,
+(select @total) as totalCompra,
+(SELECT [dbo].[CantidadConLetra](@total)) as totalLetra,
+fv.fechaReg 
 from facturasVentas fv 
 inner join pedidoVenta pv 
 on fv.pedidoVenta_idPedido = pv.idPedido
 inner join detallePedido dp 
 on pv.idPedido = dp.pedidoVenta_idPedido
---where fv.idFacturaVenta = @idFactura
-ORDER by dp.pedidoVenta_idPedido 
+where fv.idFacturaVenta = @idFactura
+
+
+
 
 
 
